89789f07fa240b883ccfb2d95bde41f74ea87703
Jeff Wymore <yardboy2401@gmail.com>
2021-10-23 19:37:19 -0700

    after final updates and comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,101 +1,185 @@
-let contentEl = $("#theContent");
-let userQueryEl = $("#userQuery");
-let searchBtnEl = $("#searchBtn");
-let currentWeatherEl = $("#currentWeather")
-let extendedForecastEl = $("#extendedForecast")
-let locRequestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
-let localRequestUrlTwo = "https://api.openweathermap.org/data/2.5/onecall?";
-let apiKey = "a4d4fbcf65c30db41df7a19f3e5dba8f&units=imperial"
-let searchHistoryEl = $("#searchHistory")
-let searchHistoryList = $("#searchHistoryList")
+//document ready on load function
+$(document).ready(function () {
+    //define global variables
+  let contentEl = $("#theContent");
+  let userQueryEl = $("#userQuery");
+  let searchBtnEl = $("#searchBtn");
+  let currentWeatherEl = $("#currentWeather");
+  let extendedForecastEl = $("#extendedForecast");
+  let locRequestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
+  let localRequestUrlTwo = "https://api.openweathermap.org/data/2.5/onecall?";
+  let apiKey = "a4d4fbcf65c30db41df7a19f3e5dba8f&units=imperial";
+  let searchHistoryEl = $("#searchHistory");
+  let searchHistoryListEl = $("#searchHistoryList");
 
-function fetchUserQuery(fetchRequestUrl) {
+    //get local storage city data
+  function getLocalStorageCities() {
+    let searchedCities =
+      JSON.parse(localStorage.getItem("searchedCities")) || [];
+    return searchedCities || [];
+  }
+  //clears previous forecast data if present
+  function clearWeatherData() {
+    $("#weatherIconData").empty();
+    $("#extWeatherIconData1").empty();
+    $("#extWeatherIconData2").empty();
+    $("#extWeatherIconData3").empty();
+    $("#extWeatherIconData4").empty();
+    $("#extWeatherIconData5").empty();
+    $("#extendedForecastCard1").empty();
+    $("#extendedForecastCard2").empty();
+    $("#extendedForecastCard3").empty();
+    $("#extendedForecastCard4").empty();
+    $("#extendedForecastCard5").empty();
+  }
+
+    //API calls for forecast data-#1 for current weather and #2 for ext forecast data
+  function fetchUserQuery(fetchRequestUrl) {
     fetch(fetchRequestUrl, {
-        // options
+      // options
     })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+          //adding current weather data and weather icon to page
+        let currentDateTime = moment().format("dddd, MMMM Do, YYYY");
+        currentWeatherEl.prepend($("<h2>").text("Date: " + currentDateTime));
+        currentWeatherEl.prepend($("<h2>").text("City: " + data.city.name));
+        currentWeatherEl.prepend($("<h2>").text("Current Weather"));
+        var weatherIconData = $('<img id="weatherIconData">');
+        weatherIconData.attr(
+          "src",
+          "http://openweathermap.org/img/w/" +
+            data.list[0].weather[0].icon +
+            ".png"
+        );
+        weatherIconData.appendTo($("#currentWeather"));
+        currentWeatherEl.append(
+          $("<h2>").text("Temp: " + data.list[1].main.temp + " deg F")
+        );
+        currentWeatherEl.append(
+          $("<h2>").text("Wind: " + data.list[0].wind.speed + " MPH")
+        );
+        currentWeatherEl.append(
+          $("<h2>").text("Humidity: " + data.list[0].main.humidity + " %")
+        );
+        let cityLat = data.city.coord.lat;
+        let cityLon = data.city.coord.lon;
+        let secondCurQuery =
+          localRequestUrlTwo +
+          "lat=" +
+          cityLat +
+          "&lon=" +
+          cityLon +
+          "&exclude=minutely,hourly&appid=" +
+          apiKey;
+        fetchUserQueryTwo(secondCurQuery);
+      });
+
+    function fetchUserQueryTwo(fetchRequestUrlTwo) {
+      fetch(fetchRequestUrlTwo, {})
         .then(function (response) {
-            return response.json();
+          return response.json();
         })
         .then(function (data) {
-            // console.log(data);
-            let currentDateTime = moment().format("dddd, MMMM Do, YYYY")
-            currentWeatherEl.prepend($("<h2>").text("Date: " + currentDateTime));
-            currentWeatherEl.prepend($("<h2>").text("City: " + data.city.name));
-            currentWeatherEl.prepend($("<h2>").text("Current Weather"));
-            currentWeatherEl.append($("<h2>").text("Temp: " + data.list[1].main.temp + " deg F"));
-            currentWeatherEl.append($("<h2>").text("Wind: " + data.list[0].wind.speed + " MPH"));
-            currentWeatherEl.append($("<h2>").text("Humidity: " + data.list[0].main.humidity + " %"));
-            $("#weatherIconData").attr("src", "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png");
-            let cityLat = data.city.coord.lat;
-            let cityLon = data.city.coord.lon;
-            let secondCurQuery = localRequestUrlTwo + "lat=" + cityLat + "&lon=" + cityLon + "&exclude=minutely,hourly&appid=" + apiKey;
-            // console.log(secondCurQuery) 
-            fetchUserQueryTwo(secondCurQuery);
-            
+          currentWeatherEl.append(
+            $("<h2>")
+              .text("UV Index: " + data.current.uvi)
+              .attr("id", "uvi")
+          );
+            //changing UV Index font color based on severity of index number
+          if (data.current.uvi <= 3) {
+            $("#uvi").css({ color: "green", "font-weight": "bolder" });
+          } else if (data.current.uvi > 3 && data.current.uvi <= 7) {
+            $("#uvi").css({ color: "yellow", "font-weight": "bolder" });
+          } else {
+            $("#uvi").css({ color: "red", "font-weight": "bolder" });
+          }
+          //populating extended forecast cards data
+          for (let i = 1; i < data.daily.length - 2; i++) {
+            const element = data.daily[i];
+            $("#extendedForecastCard" + [i]).prepend(
+              $("<h2>").text(moment(data.daily[i].dt * 1000).format("MM/DD/YY"))
+            );
+            var extWeatherIconData = $('<img id="extWeatherIconData">');
+            extWeatherIconData.attr("id", "extWeatherIconData" + [i]);
+            extWeatherIconData.attr(
+              "src",
+              "http://openweathermap.org/img/w/" +
+                data.daily[i].weather[0].icon +
+                ".png"
+            );
+            extWeatherIconData.appendTo($("#extendedForecastCard" + [i]));
+            $("#extendedForecastCard" + [i]).append(
+              $("<h2>").text("Temp " + data.daily[i].temp.day + " deg F")
+            );
+            $("#extendedForecastCard" + [i]).append(
+              $("<h2>").text("Wind: " + data.daily[i].wind_speed + " MPH")
+            );
+            $("#extendedForecastCard" + [i]).append(
+              $("<h2>").text("Humidity: " + data.daily[i].humidity + "%")
+            );
+          }
         });
+    }
+  }
 
-        function fetchUserQueryTwo(fetchRequestUrlTwo) {
-            fetch(fetchRequestUrlTwo, {
-                // opt ions
-            })
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    console.log(data);
-                    
-                    // contentEl.append($("<h2>").text("Lat: " + data.lat));
-                    // contentEl.append($("<h2>").text("Lon: " + data.lon));
-                    currentWeatherEl.append($("<h2>").text("UV Index: " + data.current.uvi));
+//display locally stored city data upon page load
+  function displayLocalStorageCities() {
+    searchHistoryListEl.html("");
+    let searchedCities =
+      JSON.parse(localStorage.getItem("searchedCities")) || [];
+    searchedCities.forEach((searchedCities) => {
+      let liEl = $("<li class='searchedCities'>");
+      liEl.text(searchedCities);
+      searchHistoryListEl.append(liEl);
+    });
+  }
+  displayLocalStorageCities();
 
-                    for (let i = 1; i < (data.daily.length -2); i++) {
-                        const element = data.daily[i];
-                        $("#extendedForecastCard" + [i]).prepend($("<h2>").text(moment(data.daily[i].dt * 1000).format("MM/DD/YY")));
-                        $("#extWeatherIconData" + [i]).attr("src", "http://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png");
-                        $("#extendedForecastCard" + [i]).append($("<h2>").text("Temp " + data.daily[i].temp.day + " deg F"));
-                        $("#extendedForecastCard" + [i]).append($("<h2>").text("Wind: " + data.daily[i].wind_speed + " MPH"));
-                        $("#extendedForecastCard" + [i]).append($("<h2>").text("Humidity: " + data.daily[i].humidity + "%"));
-
-                    }
-                    
-                });
-        }
-}
-
-searchBtnEl.click(() => {
+  //on click function for searched city history list
+  $(document).on("click", ".searchedCities", function () {
+    clearWeatherData();
+    $(this).text();
     currentWeatherEl.css("display", "flex");
     extendedForecastEl.css("display", "block");
-    let curQuery = locRequestUrl + userQueryEl.val() + "&appid=" + apiKey
+    currentWeatherEl.empty();
+    let searchCity = locRequestUrl + $(this).text() + "&appid=" + apiKey;
+    console.log(searchCity);
+    fetchUserQuery(searchCity);
+  });
+
+  //get local storage city data
+  function getLocalStorageCities() {
+    let searchedCities =
+      JSON.parse(localStorage.getItem("searchedCities")) || [];
+    return searchedCities || [];
+  }
+
+  //search button click function setting displays creating api key and appending search to search history
+  searchBtnEl.click(() => {
+    clearWeatherData();
+    currentWeatherEl.css("display", "flex");
+    extendedForecastEl.css("display", "block");
+    currentWeatherEl.empty();
+    let curQuery = locRequestUrl + userQueryEl.val() + "&appid=" + apiKey;
     console.log(curQuery);
-    let searchedCities = JSON.parse(localStorage.getItem("searchedCities")) || [];
-    searchedCities.push(userQueryEl.val());
-    localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
+    let searchedCities = getLocalStorageCities();
+    console.log(getLocalStorageCities());
+    if (
+      !searchedCities ||
+      !searchedCities.find((m) => m === userQueryEl.val().toLowerCase())
+    ) {
+      searchedCities.push(userQueryEl.val().toLowerCase());
+      console.log(userQueryEl.val());
+      localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
+      searchHistoryListEl.append(
+        $("<li class='searchedCities'>").text(userQueryEl.val().toLowerCase())
+      );
+      userQueryEl.val("");
+    }
 
-    // localStorage.setItem("city", userQueryEl.val());
-    // localStorage.setItem("searchedCities", JSON.stringify(["seattle", "portland", "wenatchee"]));
-    // console.log(JSON.parse(localStorage.getItem("searchedCities")).push(userQueryEl.val()))
-    $("#searchHistory").append($("<li>").text(userQueryEl.val()));
-    // $(this).val(localStorage.getItem($(this).attr('searchedCities')))
-    $('#userQuery').val("");
     fetchUserQuery(curQuery);
+  });
 });
-
-
-jQuery(document).ready(function() {
-    $('#searchHistoryList').each(function() {
-    $(this).val(localStorage.getItem($(this).attr('searchedCities')))
-    // console.log($(this).val())
-   })
-})
-
-
-
-// $.each(recentSearches, function (index, value) {
-//     $('#searchHistory').append("<li class='historyItem'  onclick='addtotextbox("+index+")'>" + value + '</li>');
-// });
-// }
-
-// function addtotextbox(id)
-// {
-// $('#textboxSearch').val(recentSearches[id]);
-// }
